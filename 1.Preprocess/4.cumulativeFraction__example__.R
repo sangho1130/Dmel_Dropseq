9bb9fb4d9933b4af206c4574fd9302e144ac7456
sangho1130 <56675654+sangho1130@users.noreply.github.com>
2019-11-26 20:32:23 +0900

    Set a cell count cutoff value

--- 1.Preprocess/4.cumulativeFraction__example__.R
+++ 1.Preprocess/4.cumulativeFraction__example__.R
@@ -3,6 +3,7 @@
 a <- read.table("read count table", header = FALSE, stringsAsFactors = FALSE)
 x <- cumsum(a$V1)
 x <- x/max(x)
+
 plot(1:length(x), x, type='l', col="blue", 
      xlab = "cell barcodes sorted by number of reads\n[descending]",
      ylab = "cumulative fraction of reads", 
