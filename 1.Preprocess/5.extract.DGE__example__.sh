e91b42ddad16d8e8d6e338420de9e3048377b6c9
sangho1130 <56675654+sangho1130@users.noreply.github.com>
2019-11-26 20:32:55 +0900

    Make an UMI count matrix

--- 1.Preprocess/5.extract.DGE__example__.sh
+++ 1.Preprocess/5.extract.DGE__example__.sh
@@ -3,4 +3,3 @@
 
 # 4. UMI count table
 # <path to Drop-seq tools>/DigitalExpression I=<aligned bam> O=<output UMI table> SUMMARY=<output summary> MIN_NUM_READS_PER_CELL=5000 (or other thresholds)
-
