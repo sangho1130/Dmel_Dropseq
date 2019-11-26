2a356d10d925a7a1f28980466cf1c6ef7d8b7a47
sangho1130 <56675654+sangho1130@users.noreply.github.com>
2019-11-26 20:23:09 +0900

    Delete 5.extract.DGE__example__.sh

--- 5.extract.DGE__example__.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-#cd $PBS_O_WORKDIR
-
-# 4. UMI count table
-# <path to Drop-seq tools>/DigitalExpression I=<aligned bam> O=<output UMI table> SUMMARY=<output summary> MIN_NUM_READS_PER_CELL=5000 (or other thresholds)
-
