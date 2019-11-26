b297256b778b2e6b92a61b56a84193d0aff95598
sangho1130 <56675654+sangho1130@users.noreply.github.com>
2019-11-26 20:29:14 +0900

    Described in DropSeq Alignment Cookbook

---
+++ 1.Preprocess/metafiles/1.picard.creatsequencedictionary.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+#cd $PBS_O_WORKDIR
+
+# java -jar <path to Drop-seq tools>/3rdParty/picard/picard.jar CreateSequenceDictionary R=<~.fasta genome reference> O=<~.dict output>
---
+++ 1.Preprocess/metafiles/2.convertToRefFalt.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+# cd $PBS_O_WORKDIR
+
+# <path to Drop-seq tools>/ConvertToRefFlat ANNOTATIONS_FILE=<gtf annotation> SEQUENCE_DICTIONARY=<~.dict metafile> O=<~.refFlat output>
