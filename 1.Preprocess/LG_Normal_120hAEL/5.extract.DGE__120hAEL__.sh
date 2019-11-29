8195ff59e0499d2ea4f7cf138d19f494712c9a09
sangho1130 <56675654+sangho1130@users.noreply.github.com>
2019-11-29 15:56:36 +0900

    Add files via upload

---
+++ 1.Preprocess/LG_Normal_120hAEL/5.extract.DGE__120hAEL__.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+cd $PBS_O_WORKDIR
+
+### 120 h AEL lib1 ###
+/home/sangho/programs/src/Drop-seq_tools-1.13/DigitalExpression I=lib1/error_detected.bam O=lib1/error_detected.expr.txt SUMMARY=lib1/error_detected.summary.txt MIN_NUM_READS_PER_CELL=5000
+
+### 120 h AEL lib2 ###
+/home/sangho/programs/src/Drop-seq_tools-1.13/DigitalExpression I=lib2/error_detected.bam O=lib2/error_detected.expr.txt SUMMARY=lib2/error_detected.summary.txt MIN_NUM_READS_PER_CELL=5000
+
+### 120 h AEL lib3 ###
+/home/sangho/programs/src/Drop-seq_tools-1.13/DigitalExpression I=lib3/error_detected.bam O=lib3/error_detected.expr.txt SUMMARY=lib3/error_detected.summary.txt MIN_NUM_READS_PER_CELL=5000
+
+### 120 h AEL lib4 ###
+/home/sangho/programs/src/Drop-seq_tools-1.13/DigitalExpression I=lib4/error_detected.bam O=lib4/error_detected.expr.txt SUMMARY=lib4/error_detected.summary.txt MIN_NUM_READS_PER_CELL=5000
+
